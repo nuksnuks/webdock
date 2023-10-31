3918dd78264d022291b9c3928a293e6c72f5c7fa
nuksnuks <95483927+nuksnuks@users.noreply.github.com>
2023-10-31 22:39:08 +0100

    initial
    installeret react, sass og lavet mappestruktur:
    
    - assets (til medier)
    - Components (til componenter)
    - styles (til sass)

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,14 @@
+import '../styles/App.scss'
+
+function App() {
+
+  return (
+    <>
+      <div >
+        <h2>Dette er 'App' Componentet</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/NytComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NytComponent = () => {
+
+
+  return (
+    <div>Dette er et nyt component</div>
+  )
+}
+
+export default NytComponent
