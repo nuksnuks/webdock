85a3c7316bd332cd47aa77582833a8d344b911ce
Nicole le Fevre <83703145+nicolelefevre98@users.noreply.github.com>
2023-11-08 13:18:45 +0100

    Posts
    Posts v1 - dummydata & placeholders

--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import '/./src/styles/post.scss'; // Importer SCSS-filen
 
-const Posts = () => {
+const PostList = () => {
+  const [posts, setPosts] = useState([]);
+  const [showDummyData, setShowDummyData] = useState(true);
+
+  useEffect(() => {
+    // Udfør asynkron API-anmodning her
+    fetch('din_api_endpoint_url')
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+        setShowDummyData(false); // Skjul dummydata, når de faktiske data er tilgængelige
+      })
+      .catch((error) => {
+        console.error('Fejl ved hentning af postdata:', error);
+      });
+  }, []); // Tømme afhængighedsarray, så dette kun kører ved montering
+
   return (
-    <div>Posts</div>
-  )
-}
+    <div className='post-list'>
+      <h2>Post Oversigt</h2>
+      {showDummyData ? (
+        <ul>
+          {[1, 2, 3].map((id) => (
+            <li key={id}>Placeholder Post {id}</li>
+          ))}
+        </ul>
+      ) : (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
-export default Posts
+export default PostList;
