e58f7dcada2d04dfa492f73510cb951e55f40995
Maria Nordahl Dimon <momeria.dimon@gmail.com>
2023-11-08 12:53:59 +0100

    Roadmap
    Layout og SCSS til Roadmap oprettet.
    Post skal tilføjes dynamisk.

--- frontend/src/pages/Roadmap.jsx
+++ frontend/src/pages/Roadmap.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import  '/./src/styles/Roadmap.scss'
 
 const Roadmap = () => {
     console.log("1")
     return (
     <>
-        <div>test 1</div>
-        <p>test 2</p>
+      <div className="roadmap">
+        <div className="roadmapColumn">
+          <div className="roadmapColumnPlanned"> Planned </div>
+          <div className="roadmapContent">
+          </div>
+        </div>
+        <div className="roadmapColumn">
+          <div className="roadmapColumnProgress"> In Progress </div>
+          <div className="roadmapContent">
+          </div>
+        </div>
+        <div className="roadmapColumn">
+          <div className="roadmapColumnCompleted"> Completed </div>
+          <div className="roadmapContent">
+          </div>
+        </div>
+      </div>
     </>
   )
 }
