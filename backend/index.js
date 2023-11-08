16c0378bf7df0d23428d05d8913afd3d3a76bab5
nuksnuks <95483927+nuksnuks@users.noreply.github.com>
2023-11-08 11:40:36 +0100

    backend
    Backend:
    - opsat og klar til at anvendes.
    - første endpoint er bare på "/"
    
    Frontend:
    rettelser i nav, "frontpage"-linket førte til single siden

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+var app = express();
+
+app.get('/', (req, res) => {
+    res.json("this is backend")
+});
+
+
+//server kører på port 
+app.listen(3301, ()=>{
+    console.log("backend kører på http://localhost:3301/")
+});
