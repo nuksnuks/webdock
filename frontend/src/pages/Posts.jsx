bd933697344d65bf9428f443b72760eae148fd50
Nicole le Fevre <83703145+nicolelefevre98@users.noreply.github.com>
2023-11-09 19:38:37 +0100

    Note update
    Description of the code with notes and i tried to add name ID and content 200 sign but changed it back cause i think i have to get the data before

--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -1,30 +1,40 @@
+// Importer useState og useEffect for at kunne oprette komponenter så vi kan bruge de her hooks
 import React, { useEffect, useState } from 'react';
 import '/./src/styles/post.scss'; // Importer SCSS-filen
 
+
+//Selve listen over de posts bliver defineret her i en arrow
 const PostList = () => {
+  //Bruger her hooks usestate
+  // Posts indeholder et array af postdata og setPosts er en funktion som bruges til at opdatere posts
   const [posts, setPosts] = useState([]);
+  //Show dummydata (Skal laves om når vi har databasen) viser dataen og setDD viser det
   const [showDummyData, setShowDummyData] = useState(true);
 
+
+  //useEffect udfører API anmodning
   useEffect(() => {
-    // Udfør asynkron API-anmodning her
+    //Fetch henter til den url vi får fra webdock.
     fetch('din_api_endpoint_url')
       .then((response) => response.json())
       .then((data) => {
+        //behandling af responsdataen og indlæses her i posts ved at kalde setPosts
         setPosts(data);
         setShowDummyData(false); // Skjul dummydata, når de faktiske data er tilgængelige
       })
       .catch((error) => {
         console.error('Fejl ved hentning af postdata:', error);
       });
-  }, []); // Tømme afhængighedsarray, så dette kun kører ved montering
+  }, []); // Tom dependency array, køres ved databsens opbygning
 
+//Her render vi komponentets indhold baseret på vores Dummy Data
   return (
     <div className='post-list'>
-      <h2>Post Oversigt</h2>
+      <h2>Activity Feed</h2>
       {showDummyData ? (
         <ul>
           {[1, 2, 3].map((id) => (
-            <li key={id}>Placeholder Post {id}</li>
+            <li key={id}> Placeholder Post {id}</li>
           ))}
         </ul>
       ) : (
