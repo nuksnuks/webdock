fe9cb889fc5658a093cc55d5e1fc22ebff492cab
Abdalrhman1989 <abdalrhmanaldarra@gmail.com>
2023-11-13 02:08:50 +0100

    controllers created
    controllers created  for models

---
+++ backend/Controllers/PostController.js
@@ -0,0 +1,32 @@
+// controllers/PostController.js
+const { Post } = require('../models');
+
+const PostController = {
+  getAllPosts: async (req, res) => {
+    try {
+      const posts = await Post.findAll();
+      res.json(posts);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  },
+
+  getPostById: async (req, res) => {
+    const postId = req.params.id;
+    try {
+      const post = await Post.findByPk(postId);
+      if (!post) {
+        return res.status(404).send('Post not found');
+      }
+      res.json(post);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  },
+
+  // Add other post-related controller methods...
+};
+
+module.exports = PostController;
