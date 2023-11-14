79aa9b7cbb5e7fe229e84d5296c6650c6f470184
Nicole le Fevre <83703145+nicolelefevre98@users.noreply.github.com>
2023-11-14 16:59:06 +0100

    this is wrong
    dont check this one

--- backend/Models/UserModel.js
+++ backend/Models/UserModel.js
@@ -40,8 +40,8 @@ const User = sequelize.define('User', {
 
 // Define associations with other models
 User.associate = (models) => {
-  User.hasMany(models.Post, { foreignKey: 'userId' });
-  User.belongsTo(models.Role, { foreignKey: 'roleId' });
+  User.hasMany(models.Post, { foreignKey: 'UserId' });
+  User.belongsTo(models.Role, { foreignKey: 'RoleId' });
   // Add other associations...
 };
 
