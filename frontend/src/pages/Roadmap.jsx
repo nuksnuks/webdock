63adfa5f5e0642b0769a5f7090d2f133bbfe642d
nuksnuks <95483927+nuksnuks@users.noreply.github.com>
2023-11-23 17:29:32 +0100

    Revert "Post Card"
    This reverts commit b9df0aadb812ec57bc1e70dd8cf58677c6a819a3.

--- frontend/src/pages/Roadmap.jsx
+++ frontend/src/pages/Roadmap.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
 import  '/./src/styles/Roadmap.scss'
-import PostCard from '../components/PostCard'
-import Post from '../../../backend/Models/PostModel'
 
 const Roadmap = () => {
     return (
     <>
       <div className="roadmap">
         <div className="roadmapColumn">
-          <div className="roadmapColumnPlanned">
-              <div className="dot"></div>
-              <h2>Planned</h2>
-          </div>
+          <div className="roadmapColumnPlanned"> Planned </div>
           <div className="roadmapContent">
-              {/* Her indsætter vi vores PostCard, og fortæller den hvilke værdier den skal sætte ind, hvor den skal finde de her værdier og hvor de skal sættes ind.  */}
-            <PostCard title={Post.title} desc={Post.description} date={Post.createdAt} likes={Post.likeStatus.length} comments={Post.commentId.length} ></PostCard>
           </div>
         </div>
         <div className="roadmapColumn">
