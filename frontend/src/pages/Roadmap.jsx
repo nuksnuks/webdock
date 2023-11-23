ac7de37a0bf8e54fa122e045a625c7b9994de9b4
Maria Nordahl Dimon <momeria.dimon@gmail.com>
2023-11-23 15:49:00 +0100

    Revert "Post Card Added"
    This reverts commit 9d6a09248485f64dcf4472eacf694e36ec4ac452.

--- frontend/src/pages/Roadmap.jsx
+++ frontend/src/pages/Roadmap.jsx
@@ -13,22 +13,20 @@ const Roadmap = () => {
               <h2>Planned</h2>
           </div>
           <div className="roadmapContent">
-            {/* Her indsætter vi vores PostCard, og fortæller den hvilke værdier den skal sætte ind, hvor den skal finde de her værdier og hvor de skal sættes ind.  */}
+              {/* Her indsætter vi vores PostCard, og fortæller den hvilke værdier den skal sætte ind, hvor den skal finde de her værdier og hvor de skal sættes ind.  */}
             <PostCard title={Post.title} desc={Post.description} date={Post.createdAt} likes={Post.likeStatus.length} comments={Post.commentId.length} ></PostCard>
           </div>
         </div>
         <div className="roadmapColumn">
           <div className="roadmapColumnProgress"> In Progress </div>
           <div className="roadmapContent">
-            {/* Her indsætter vi vores PostCard, og fortæller den hvilke værdier den skal sætte ind, hvor den skal finde de her værdier og hvor de skal sættes ind.  */}
-            <PostCard title={Post.title} desc={Post.description} date={Post.createdAt} likes={Post.likeStatus.length} comments={Post.commentId.length} ></PostCard>
           </div>
         </div>
         <div className="roadmapColumn">
           <div className="roadmapColumnCompleted"> Completed </div>
           <div className="roadmapContent">
-            {/* Her indsætter vi vores PostCard, og fortæller den hvilke værdier den skal sætte ind, hvor den skal finde de her værdier og hvor de skal sættes ind.  */}
-            <PostCard title={Post.title} desc={Post.description} date={Post.createdAt} likes={Post.likeStatus.length} comments={Post.commentId.length} ></PostCard>
+          </div>
+          <div className="roadmapContent">
           </div>
         </div>
       </div>
