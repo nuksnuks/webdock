7367de974edd3c425b6e1af333fa302331eed6ce
Maria Nordahl Dimon <momeria.dimon@gmail.com>
2023-11-23 15:48:36 +0100

    Revert "Basic component insert"
    This reverts commit 6affddfed339522e967bf0e4187718ea73f18b0c.

--- frontend/src/pages/Roadmap.jsx
+++ frontend/src/pages/Roadmap.jsx
@@ -2,39 +2,36 @@ import React from 'react'
 import  '/./src/styles/Roadmap.scss'
 import PostCard from '../components/PostCard'
 import Post from '../../../backend/Models/PostModel'
-import Components from 'components'
 
 const Roadmap = () => {
     return (
     <>
-      <Components>
-        <div className="roadmap">
-          <div className="roadmapColumn">
-            <div className="roadmapColumnPlanned">
-                <div className="dot"></div>
-                <h2>Planned</h2>
-            </div>
-            <div className="roadmapContent">
-              {/* Her indsætter vi vores PostCard, og fortæller den hvilke værdier den skal sætte ind, hvor den skal finde de her værdier og hvor de skal sættes ind.  */}
-              <PostCard title={Post.title} desc={Post.description} date={Post.createdAt} likes={Post.likeStatus.length} comments={Post.commentId.length} ></PostCard>
-            </div>
+      <div className="roadmap">
+        <div className="roadmapColumn">
+          <div className="roadmapColumnPlanned">
+              <div className="dot"></div>
+              <h2>Planned</h2>
           </div>
-          <div className="roadmapColumn">
-            <div className="roadmapColumnProgress"> In Progress </div>
-            <div className="roadmapContent">
-              {/* Her indsætter vi vores PostCard, og fortæller den hvilke værdier den skal sætte ind, hvor den skal finde de her værdier og hvor de skal sættes ind.  */}
-              <PostCard title={Post.title} desc={Post.description} date={Post.createdAt} likes={Post.likeStatus.length} comments={Post.commentId.length} ></PostCard>
-            </div>
-          </div>
-          <div className="roadmapColumn">
-            <div className="roadmapColumnCompleted"> Completed </div>
-            <div className="roadmapContent">
-              {/* Her indsætter vi vores PostCard, og fortæller den hvilke værdier den skal sætte ind, hvor den skal finde de her værdier og hvor de skal sættes ind.  */}
-              <PostCard title={Post.title} desc={Post.description} date={Post.createdAt} likes={Post.likeStatus.length} comments={Post.commentId.length} ></PostCard>
-            </div>
+          <div className="roadmapContent">
+            {/* Her indsætter vi vores PostCard, og fortæller den hvilke værdier den skal sætte ind, hvor den skal finde de her værdier og hvor de skal sættes ind.  */}
+            <PostCard title={Post.title} desc={Post.description} date={Post.createdAt} likes={Post.likeStatus.length} comments={Post.commentId.length} ></PostCard>
           </div>
         </div>
-      </Components>
+        <div className="roadmapColumn">
+          <div className="roadmapColumnProgress"> In Progress </div>
+          <div className="roadmapContent">
+            {/* Her indsætter vi vores PostCard, og fortæller den hvilke værdier den skal sætte ind, hvor den skal finde de her værdier og hvor de skal sættes ind.  */}
+            <PostCard title={Post.title} desc={Post.description} date={Post.createdAt} likes={Post.likeStatus.length} comments={Post.commentId.length} ></PostCard>
+          </div>
+        </div>
+        <div className="roadmapColumn">
+          <div className="roadmapColumnCompleted"> Completed </div>
+          <div className="roadmapContent">
+            {/* Her indsætter vi vores PostCard, og fortæller den hvilke værdier den skal sætte ind, hvor den skal finde de her værdier og hvor de skal sættes ind.  */}
+            <PostCard title={Post.title} desc={Post.description} date={Post.createdAt} likes={Post.likeStatus.length} comments={Post.commentId.length} ></PostCard>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
