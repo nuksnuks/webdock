850403d64b9377e54cf65884e891131ef5ab02f1
emilstorta <emilstorta@users.noreply.github.com>
2023-11-24 11:56:34 +0100

    singlePostCard
    Created a new component SinglePostCard.
    
    Post.jsx has been updated to import this component.
    
    There are still some errors to be corrected around only showing one list item from the dummyData.json.
    
    Styling has not been updated and will be looked at.
    
    Co-Authored-By: Maria Nordahl Dimon <83703270+M-Dimon@users.noreply.github.com>
    Co-Authored-By: Nicole le Fevre <83703145+nicolelefevre98@users.noreply.github.com>

--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -1,84 +1,33 @@
-// Importer useState og useEffect for at kunne oprette komponenter så vi kan bruge de her hooks
-import React, { useEffect, useState } from 'react';
-import '/./src/styles/post.scss'; // Importer SCSS-filen
+import React from 'react';
+import '/./src/styles/post.scss';
+import SinglePostCard from '../components/SinglePostCard';
+import jsonData from '../assets/data/dummyData.json';
 
 
-
-
-//Selve listen over de posts bliver defineret her i en arrow
-const PostList = () => {
-  //Bruger her hooks usestate
-  // Posts indeholder et array af postdata og setPosts er en funktion som bruges til at opdatere posts
-  const [posts, setPosts] = useState([]);
-  //Show dummydata (Skal laves om når vi har databasen) viser dataen og setDD viser det
-  const [showDummyData, setShowDummyData] = useState(true);
-
-
-  fetch('http://localhost:3066/users', {mode: 'cors'})
-  .then(userdata => userdata.json())
-  .then(response => console.log(response))
-  .catch(error => console.error('Error:', error));
-
-  //useEffect udfører API anmodning
-  useEffect(() => {
-    //Fetch henter url
-    fetch('/Post')
-      .then((response) => response.json())
-      .then((data) => {
-        //behandling af responsdataen og indlæses her i posts ved at kalde setPosts
-        setPosts(data)
-        
-        setShowDummyData(false); // Skjul dummydata, når de faktiske data er tilgængelige
-      
-        //Generer og indstiller dummy brugernavne
-        const dummyUsernames = ["UserA", "UserB", "UserC"]; // Erstat med dine egne dummy brugernavne
-      setUsernames(dummyUsernames);
-      })
-      .catch((error) => {
-        console.error('Fejl ved hentning af postdata:', error);
-      });
-  }, []); // Tom dependency array, køres ved databasen opbygning
-
-//Her render vi komponentets indhold baseret på vores Dummy Data
+const Post = () => {
+  
   return (
-    <div className='post-list'>
+    <>
       
-      <div className='request'>
-      <div className="vote-section">
-                  <button className='arrow'></button>
-                  <span>2</span>
-                  <button className='arrow'></button>
-                </div>
-                <div>
-        <h1>Feature request Title</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus.</p>
-                <p>Created by ... on 00/00/0000</p>
-                </div>
-      </div>
-      <h2>Activity Feed</h2> {showDummyData ? (
-        <div className='post-container'>
-          <ul>
-            {[1, 2, 3].map((id) => (
-              <li key={id} className='post-item'>
-              <img src="/src/assets/avatar.jpg" alt="" className="user-image"/>
-              <div className='post-content'>
-              <h3>Placeholder til comment {id}</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus.</p>
-              </div>
-              </li>
+
+        {jsonData.map((item) => (
+            <div className="request">
+              <SinglePostCard userID={item.userID}
+      userImg={item.userImg}
+      status={item.status}
+      title={item.title}
+      desc={item.desc}
+      date={item.date}
+      likes={item.likes.length} />
+            </div>
             ))}
-          </ul>
-        </div>
         
-      ) : (
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id}>{post.title}</li>
-          ))}
-        </ul>
-      )}
-    </div>
+    
+    
+    
+    
+    </>
   );
 };
 
-export default PostList;
+export default Post
