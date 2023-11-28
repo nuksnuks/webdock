18b4acc51536a74b7fe5dfa8b5f5c453f28a733b
Nicole le Fevre <83703145+nicolelefevre98@users.noreply.github.com>
2023-11-28 21:17:02 +0100

    Post is fetching!
    Post is fetching fron our correct endpoint and is working

--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '/./src/styles/post.scss';
 import SinglePostCard from '../components/SinglePostCard';
-import jsonData from '../assets/data/dummyData.json';
 
 const Post = () => {
-  const targetUserID = "1";
+  //const targetUserID = 13; //Target id 1 da vi ikke har SSO endnu
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from backend API
+    fetch('http://localhost:3001/post')
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.log('Error fetching data:', error));
+  }, []); 
 
   return (
     <>
-      {jsonData.filter(item => item.userID === targetUserID).map((item) => (
+      {posts.filter(item => item.postID === 13).map((item) => (
         <SinglePostCard 
           userID={item.userID}
           userImg={item.userImg}
           status={item.status}
           title={item.title}
-          desc={item.desc}
+          desc={item.description}
           date={item.date}
-          likes={item.likes.length} />
+          likes={item.likes} />
       ))}
     </>
   );
