e978a35c551a00130800cca51711609e17552213
Maria Nordahl Dimon <momeria.dimon@gmail.com>
2023-11-29 13:36:50 +0100

    Back button for post and Create request corrected

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -7,6 +7,7 @@ const Layout = () => {
   const location = useLocation();
   const isGlobalComponentPage = location.pathname === '/GlobalComponent';
   const isPostPage = location.pathname === '/posts';
+  const isPostPage2 = location.pathname === '/CreateFeatureRequest';
 
   return (
     <>
@@ -14,14 +15,15 @@ const Layout = () => {
       <div className="FrontPage">
             <div className="wrapper">
               <div className="title-container">
-                {!isPostPage && (
+                {!isPostPage && !isPostPage2  && (
                 <>
                 <h1 className={`title1 ${isGlobalComponentPage ? 'global-component-page' : ''}`}><Link to="/roadmap">Roadmap</Link></h1>
                 <h1 className={`title2 ${isGlobalComponentPage ? 'global-component-page' : ''}`}><Link to="/GlobalComponent">Feature Requests</Link></h1>
                 </>
                 )}
-                {isPostPage && <h1 className="title3"><Link to="/roadmap">Back</Link></h1>}
-
+                {(isPostPage || isPostPage2) && (
+                  <h1 className="title3"><Link to="/roadmap">Back</Link></h1>
+                )}
               </div>
               <div className="main-container">
               <Outlet/>
