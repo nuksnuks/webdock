94d377e562c23d44973e534c69ccd6acb1fbeeba
emilstorta <emilstorta@users.noreply.github.com>
2023-12-17 15:06:25 +0100

    Like function frontend

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import '/./src/styles/PostCard.scss'
 import { FaHeart, FaRegHeart, FaRegCommentAlt } from "react-icons/fa";
+import { useParams } from "react-router-dom";
+
 
 
 
 const PostCard = ({status, title, desc, date, likes, comments, userName, avatar }) => {
-    
+    const [liked, setLiked] = useState(false);
+    const {id} = useParams("post/:id")
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/post/${id}/like`)
+            .then((liked) => console.log(JSON.stringify(liked)))
+            .then(data => setLiked(liked));
+    }, [title]);
+
+    const handleLike = () => {
+        setLiked(!liked);
+        fetch(`http://localhost:3001/post/${id}/like`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ postId: id })
+        });
+    }
+    console.log(liked)
   return (
     <>
         
         <div className="CardBox">
             <div className='heartInfo'>
-                <div className="LikeFunc">
-                    <FaHeart className="filledHeart"/>
-                    <FaRegHeart className="heart"/>
+                <div className="LikeFunc" onClick={handleLike}>
+                    {liked ? <FaHeart className="filledHeart"/> : <FaRegHeart className="heart"/>}
                     <p>{likes}</p>
                 </div>
 
